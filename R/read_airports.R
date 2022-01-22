a2cca6ca122a24ee62ad1c90bf89678b2aef0730
rafapereirabr <rafa.pereira.br@gmail.com>
2022-01-22 17:42:10 -0300

    New function read_airports() . Issue #4

---
+++ R/read_airports.R
@@ -0,0 +1,51 @@
+#' Download airports data from Brazil
+#'
+#' @description
+#' Download data of all airports and aerodromes registered in Brazil’s Civil
+#' Aviation Agency (ANAC). Data source: \url{https://www.gov.br/anac/pt-br/assuntos/regulados/aerodromos/lista-de-aerodromos-civis-cadastrados}.
+#' The data dictionary for public airports can be found at \url{https://www.anac.gov.br/acesso-a-informacao/dados-abertos/areas-de-atuacao/aerodromos/lista-de-aerodromos-publicos-v2/70-lista-de-aerodromos-publicos-v2}.
+#' The data dictionary for private airports can be found at \url{https://www.anac.gov.br/acesso-a-informacao/dados-abertos/areas-de-atuacao/aerodromos/lista-de-aerodromos-privados-v2}.
+#'
+#' @param type String. Whether function should download data  on `public` (Default)
+#'             or `private` airports.
+#' @param showProgress Logical. Defaults to `TRUE` display progress.
+#' @return A `"data.table" "data.frame"` object.
+#' @export
+#' @family download airport data
+#' @examples \dontrun{ if (interactive()) {
+#' # Read airports data
+#' public_airports <- read_airports(type = 'public')
+#'
+#' private_airports <- read_airports(type = 'private')
+#'}}
+read_airports <- function(type = 'public', showProgress = TRUE){
+
+### check inputs
+  if( ! type %in% c('public', 'private') ){ stop(paste0("Argument 'type' must be either 'public' or 'private'")) }
+  if( ! is.logical(showProgress) ){ stop(paste0("Argument 'showProgress' must be either 'TRUE' or 'FALSE.")) }
+
+  # data url
+  url_public <- 'https://www.gov.br/anac/pt-br/assuntos/regulados/aerodromos/cadastro-de-aerodromos/aerodromos-cadastrados/cadastro-de-aerodromos-civis-publicos.csv'
+  url_private <- 'https://sistemas.anac.gov.br/dadosabertos/Aerodromos/Lista%20de%20aer%C3%B3dromos%20privados/Aerodromos%20Privados/AerodromosPrivados.csv'
+
+### download public airports
+if (type=='public'){
+  dt_public <- data.table::fread(url_public, skip = 2, encoding = 'UTF-8', showProgress=showProgress)
+
+  # fix column names
+  data.table::setnames(dt_public, unlist(c(dt_public[1,])) )
+  dt_public <- dt_public[-1,]
+  return(dt_public)
+  }
+
+### download private airports
+else if (type=='private'){
+
+  dt_private <- data.table::fread(url_private, skip = 1, showProgress=showProgress)
+
+  ## last time the data was updated
+  # last_update_private <- data.table::fread(url_private, nrows = 1)
+  # last_update_private[1,]
+  return(dt_private)
+  }
+}
---
+++ tests/testthat/test_read_airports.R
@@ -0,0 +1,28 @@
+context("read_airports")
+
+# skip tests because they take too much time
+skip_if(Sys.getenv("TEST_ONE") != "")
+testthat::skip_on_cran()
+
+
+# Reading the data -----------------------
+
+test_that("read_airports", {
+
+  # (default), one month, basica, progress
+
+    testthat::expect_true(is(read_airports(type = 'public'), "data.table"))
+    testthat::expect_true(is(read_airports(type = 'private'), "data.table"))
+    testthat::expect_true(is(read_airports(showProgress = FALSE), "data.table"))
+})
+
+
+# ERRORS and messages  -----------------------
+test_that("read_airports", {
+
+  testthat::expect_error(read_airports(type=NULL))
+  testthat::expect_error(read_airports(type='banana'))
+  testthat::expect_error(read_airports(showProgress='banana'))
+
+
+})
