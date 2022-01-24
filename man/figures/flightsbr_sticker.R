8ce01d0bb3c256945326d81a36ed1486b179fbe5
rafapereirabr <rafa.pereira.br@gmail.com>
2022-01-23 23:33:26 -0300

    add svg logo

--- man/figures/flightsbr_sticker.R
+++ man/figures/flightsbr_sticker.R
@@ -54,17 +54,14 @@ sticker(img,
 
 
 # ### .svg
-#
-#
-# sticker(img,
-#         package="flightsbr", p_family='Montserrat', p_color = '#006890',  p_fontface='italic',
-#         p_size=20, p_y = 1.6, # package name size and position
-#         s_x=1.05, s_y=.94, s_width=0.69,  # image size and position
-#         h_fill="white", h_color="#006890", h_size = 2, # hexagon
-#         dpi=400, # resolution
-#         url = 'https://github.com/ipeaGIT/flightsbr',
-#         u_size=4, u_family='Montserrat', u_color = '#003E56', # url
-#         filename="man/figures/logo.svg")  # output name
-#
+sticker(img,
+        package="flightsbr", p_family='Montserrat', p_color = 'white',  p_fontface='italic',
+        p_size=7, p_y = 1.5, # package name size and position
+        s_x=1.05, s_y=.8, s_width=.55,  # image size and position
+        h_fill="#006890", h_color="#006890", h_size = 2, # hexagon
+        dpi=400, # resolution
+        url = 'https://github.com/ipeaGIT/flightsbr',
+        u_size=1.2, u_x = 1.1, u_family='Montserrat', u_color = 'gray90', # url
+        filename="man/figures/logo.svg")  # output name
 
 
