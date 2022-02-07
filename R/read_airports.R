ba9d622386063db901dc440650d77b2786d32b36
rafapereirabr <rafa.pereira.br@gmail.com>
2022-02-07 10:46:13 -0300

    temporary fix to missing :=

--- R/read_airports.R
+++ R/read_airports.R
@@ -61,7 +61,8 @@ if (any(type %in% c('public', 'all'))){
   # dt_public[, latitude := parzer::parse_lat(latitude) ]
 
   # add type info
-  data.table::setDT(dt_public)[, type := 'public']
+  # data.table::setDT(dt_public)[, type := 'public']
+  dt_public$type <- 'public'
 
   }
 
@@ -89,7 +90,9 @@ if (any(type %in% c('private', 'all'))){
   # dt_private[, longitude := parzer::parse_lat(longitude) ]
 
   # add type info
-  data.table::setDT(dt_private)[, type := 'private']
+  # data.table::setDT(dt_private)[, type := 'private']
+  dt_private$type <- 'private'
+
   }
 
 if (type == 'private') { return(dt_private) }
