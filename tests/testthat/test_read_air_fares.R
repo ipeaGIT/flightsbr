91b33ac973cbd3e849b5db387053069bad263120
rafapereirabr <rafa.pereira.br@gmail.com>
2022-05-04 19:05:44 -0300

    add tests to read_air_fares

--- tests/testthat/test_read_air_fares.R
+++ tests/testthat/test_read_air_fares.R
@@ -6,7 +6,7 @@ testthat::skip_on_cran()
 
 
 # Reading the data -----------------------
-cols <- c('nr_ano_referencia' , 'nr_mes_referencia')
+cols <- c('Tarifa-N')
 
 test_that("read_air_fares", {
 
@@ -15,31 +15,30 @@ test_that("read_air_fares", {
   testthat::expect_true(is(test1, "data.table"))
 
   # one month, combinada, no progress bar
-  test2 <- read_flights(date=202201, type='combinada' ,showProgress = FALSE)
+  test2 <- read_air_fares(date=202201, showProgress = FALSE)
   testthat::expect_true(is(test2, "data.table"))
 
   # check content
-  testthat::expect_equal( as.character(min(test2$dt_referencia)), as.character("2022-01-01") )
+  testthat::expect_equal( as.character(min(test2$`Ano de Referência`)), as.character("2022") )
 
   # all months in a year
-  test3 <- read_flights(date=2022, select=cols, showProgress = FALSE)
+  test3 <- read_air_fares(date=2022, select=cols, showProgress = FALSE)
   testthat::expect_true(is(test3, "data.table"))
  })
 
 
 # ERRORS and messages  -----------------------
-test_that("read_flights", {
+test_that("read_air_fares", {
 
   # Wrong date 4 digits
-  testthat::expect_error(read_flights(date=1990))
-  testthat::expect_error(read_flights(date=9999))
+  testthat::expect_error(read_air_fares(date=1990))
+  testthat::expect_error(read_air_fares(date=9999))
 
   # Wrong date 6 digits
-  testthat::expect_error(read_flights(date=199001))
-  testthat::expect_error(read_flights(date=999901))
+  testthat::expect_error(read_air_fares(date=199001))
+  testthat::expect_error(read_air_fares(date=999901))
 
   # Wrong type and showProgress
-  testthat::expect_error(read_flights(type='banana'))
-  testthat::expect_error(read_flights(showProgress='banana'))
-  testthat::expect_warning(read_flights(select='banana'))
+  testthat::expect_error(read_air_fares(showProgress='banana'))
+  testthat::expect_warning(read_air_fares(select='banana'))
 })
